55625e3496fdd4cd6f1316a9dc7cd8aad1f08b08
teruyoshi_kaibara_2016 <teruyoshi_kaibara_2016@la-bit.sakura.ne.jp>
2020-01-04 14:57:31 +0900

    - initial commit with colconv mock project

---
+++ commandsByCSharp/commandsByCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace commandsByCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
