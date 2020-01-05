b561f4e85ca6366a29778c1b7b3e25fb83623f13
teruyoshi_kaibara_2016 <teruyoshi_kaibara_2016@la-bit.sakura.ne.jp>
2020-01-05 15:58:49 +0900

    - I added csclip command which pastes the text from stdin to clipboard.

---
+++ commandsByCSharp/csclip/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace csclip
+{
+    class Program
+    {
+        [STAThreadAttribute]
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            string line = string.Empty;
+
+            while ((line = Console.In.ReadLine()) != null)
+            {
+                if (sb != null)
+                {
+                    sb.Append(line + "\n");
+                }
+            }
+
+            Clipboard.SetText(sb.ToString());
+        }
+    }
+}
